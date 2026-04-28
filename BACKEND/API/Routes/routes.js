83610fc98414a5f41e50ae8adc7036a1980c9733
Korion-Brito <dev.korionbrito@gmail.com>
2026-04-28 09:30:17 -0300

    refeito routes

--- BACKEND/API/Routes/routes.js
+++ BACKEND/API/Routes/routes.js
@@ -19,12 +19,10 @@ router.post('/post', async (req, res) => {
 })
 
 //Método GET
-router.get('/getAll/:nome/:ra', async (req, res) => {
+router.get('/getAll/:', async (req, res) => {
  try {
  const resultados = await modeloTarefa.find();
- res.json({korion: nome,
-    256225: ra,
-    dados: resultados});
+ res.json(resultados)
  }
  catch (error) {
  res.status(500).json({ message: error.message })
